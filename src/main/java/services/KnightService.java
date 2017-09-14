400e19327f6cdad3cc5da3b459e33f19134a6a5a
= <katoatd@gmail.com>
2017-09-14 19:31:41 +0300

    Sort

--- src/main/java/services/KnightService.java
+++ src/main/java/services/KnightService.java
@@ -25,4 +25,23 @@ public class KnightService {
         return knight.getHelmet().getWeight()+knight.getLance().getWeight()+knight.getShield().getWeight();
     }
 	
+    public List<Knight> sortByWeight (List<Knight> knights) {
+    	Knight tempKnight = knights.get(0);
+    	boolean swapFlag = true;
+		List<Knight> sortedKnights = new ArrayList<Knight>();
+		while (swapFlag) {
+			swapFlag = false;
+			for (int i = 0; i < knights.size(); i++) {
+				if (totalWeight(knights.get(i)) > totalWeight(knights.get(i+1))) {
+					tempKnight = knights.get(i);
+					knights.set(i, knights.get(i+1));
+					knights.set(i+1, tempKnight);
+					swapFlag = true;
+				}
+			}
+		}
+		
+    	return sortedKnights;
+    	
+    }
 }
